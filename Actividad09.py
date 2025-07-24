131a7d1deb04088548a5cbcca5837affc0808019
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-07-23 19:44:03 -0600

    Actividad 9

--- Actividad09.py
+++ Actividad09.py
@@ -1,48 +1,51 @@
-Clientes = {}
+try:
+    Clientes = {}
 
-totalclientes = int(input("Ingrese el total de Clientes a Registrar:  "))
-q=1
-for i in range(totalclientes):
-    print(f"\nCliente # {q}")
-    id = int(input("Introduce el código del cliente:  "))
-    Clientes[id] = {}
-    Clientes[id]["name"] = input("Introduce el nombre del cliente:  ")
+    totalclientes = int(input("Ingrese el total de Clientes a Registrar:  "))
+    q = 1
+    for i in range(totalclientes):
+        print(f"\nCliente # {q}")
+        id = int(input("Introduce el código del cliente:  "))
+        Clientes[id] = {}
+        Clientes[id]["name"] = input("Introduce el nombre del cliente:  ")
 
-    Clientes[id]["destinos"] = {}
+        Clientes[id]["destinos"] = {}
 
-    while True:
-        totaldestinos = int(input("Ingrese cuantos destinos visitara el cliente:   "))
-        i = 1
-        if totaldestinos > 0 and totaldestinos < 6:
-            for destino in range(totaldestinos):
-                d = input(f"Introduzca el destino # {i}:   ")
-                i += 1
-                Clientes[id]["destinos"][totaldestinos] = {
-                    "viajes": d,
-                }
-            break
+        while True:
+            totaldestinos = int(input("Ingrese cuantos destinos visitara el cliente:   "))
+            i = 1
+            if totaldestinos > 0 and totaldestinos < 6:
+                for destino in range(totaldestinos):
+                    d = input(f"Introduzca el destino # {i}:   ")
+                    i += 1
+                    Clientes[id]["destinos"][totaldestinos] = {
+                        "viajes": d,
+                    }
+                break
 
-        else:
-            print("El cliente debe de tener al menos un destino y menos de 5 destinos distintos   ")
-    q=+1
+            else:
+                print("El cliente debe de tener al menos un destino y menos de 5 destinos distintos   ")
+        q = +1
 
-c=1
-for id,dato in Clientes.items():
-    print(f"\nCliente # {c}")
-    print(f"ID: {id}")
-    print(f"Nombre: {dato["name"]}")
-    print(f"Destinos:   ")
-    c+=1
-    a=1
-    for totaldestinos,data in dato["destinos"].items():
-        print(f"Destino #{a}: {data ["viajes"]}")
-        a=+1
+    c = 1
+    for id, dato in Clientes.items():
+        print(f"\nCliente # {c}")
+        print(f"ID: {id}")
+        print(f"Nombre: {dato["name"]}")
+        print(f"Destinos:   ")
+        c += 1
+        a = 1
+        for totaldestinos, data in dato["destinos"].items():
+            print(f"Destino #{a}: {data["viajes"]}")
+            a = +1
 
-def totaltrips (n):
-    if n == totalclientes:
-        return 1
-    else:
-        return Clientes[n][totaldestinos] +totaltrips(Clientes[n+1][totaldestinos])
+    total = 0
+    for id, data in Clientes.items():
+        for totaldestinos, data in data["destinos"].items():
+            total += totaldestinos
 
-clientelista = list(Clientes.keys())
-print(f"\nTotal de viajes (destinos) de todos los clientes: {totaltrips(clientelista)}")
+    print(f"Total de destinos: {total}")
+    #print(f"\nTotal de viajes (destinos) de todos los clientes: {totaltrips(totalclientes)}")
+
+except:
+    print("Hubo un Error")
